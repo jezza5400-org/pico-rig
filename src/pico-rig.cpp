6aa23a4b5e35d783de0f58054444cd02e07f754f
luke-e-gibson <me@lukegibson.dev>
2026-09-22 19:36:32 +1000

    Ensure upto date

--- src/pico-rig.cpp
+++ src/pico-rig.cpp
@@ -3,40 +3,40 @@
 
 int main()
 {
-    tusb_init();
+	tusb_init();
 
-    uint8_t rx_audio[96];
+	uint8_t rx_audio[96];
 
-    while (true)
-    {
-        tud_task();
+	while (true)
+	{
+		tud_task();
 
-        // PC -> Pico / radio TX audio
-        if (usb_audio_out_streaming())
-        {
-            uint16_t count =
-                usb_audio_read(
-                    rx_audio,
-                    sizeof(rx_audio));
+		// PC -> Pico / radio TX audio
+		if (usb_audio_out_streaming())
+		{
+			uint16_t count =
+				usb_audio_read(
+					rx_audio,
+					sizeof(rx_audio));
 
-            if (count > 0)
-            {
-                // Send rx_audio to your radio audio output.
-            }
-        }
+			if (count > 0)
+			{
+				// Send rx_audio to your radio audio output.
+			}
+		}
 
-        // Pico / radio RX audio -> PC
-        if (usb_audio_in_streaming())
-        {
-            uint8_t tx_audio[96];
+		// Pico / radio RX audio -> PC
+		if (usb_audio_in_streaming())
+		{
+			uint8_t tx_audio[96];
 
-            // Fill tx_audio from the radio.
-            //
-            // radio_audio_read(tx_audio, sizeof(tx_audio));
+			// Fill tx_audio from the radio.
+			//
+			// radio_audio_read(tx_audio, sizeof(tx_audio));
 
-            usb_audio_write(
-                tx_audio,
-                sizeof(tx_audio));
-        }
-    }
+			usb_audio_write(
+				tx_audio,
+				sizeof(tx_audio));
+		}
+	}
 }
